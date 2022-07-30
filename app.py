420e6d708386741b891babff8bfc616261337a67
Jiayi Xu <xujiayi_1256@me.com>
2022-07-30 16:38:58 +0800

    update

--- app.py
+++ app.py
@@ -3,14 +3,48 @@ import streamlit as st
 
 st.title("What's For Today?")
 
+
+cuisine_choices = [
+    "全部",
+    "日本菜",
+    "韩国料理",
+    "东南亚菜",
+    "西餐",
+    "烤肉",
+    "本帮江浙菜",
+    "川菜",
+    "粤菜",
+    "火锅",
+    "小吃快餐",
+]
+
+area_choices = [
+    "全部",
+    "徐汇区",
+    "静安区",
+    "长宁区",
+    "闵行区",
+    "浦东新区",
+    "黄浦区",
+    "普陀区",
+    "虹口区",
+    "杨浦区",
+    "宝山区",
+    "松江区",
+    "嘉定区",
+    "青浦区",
+    "奉贤区",
+    "金山区",
+    "崇明区",
+]
 st.subheader("Please choose your choice of cuisine below:")
-cuisine = st.selectbox('Cuisine', {'无', '自助餐', '中餐', '西餐'})
-if cuisine == '无':
+cuisine = st.selectbox('Cuisine', cuisine_choices)
+if cuisine == '全部':
     cuisine = None
 
 st.subheader("Please choose the area of the restaurant below:")
-area = st.selectbox('Area', {'无', '外滩', '新天地', '中山公园'})
-if area == '无':
+area = st.selectbox('Area', area_choices)
+if area == '全部':
     area = None
 
 if st.button('Show restaurants!'):
@@ -25,7 +59,8 @@ if st.button('Show restaurants!'):
     if data:
         response = requests.get("https://whats-for-today-backend.herokuapp.com/get", params=data)
         if response.ok:
-            st.header('Response: ' + str(response.json()))
+            for restaurant in response.json():
+                st.write(restaurant)
         elif response.status_code == 400:
             st.header("Something went wrong: " + str(response.json()))
     else:
